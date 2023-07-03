59e7471f9d014b62a54d27cafdbc67121ac5f42b
carlos <115598773+maninthehood@users.noreply.github.com>
2023-07-03 03:35:21 +0000

    minor updates

--- app.cpp
+++ app.cpp
@@ -1,21 +1,29 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
+#include <vector>
 
-int main(){
-
+int main()
+{
+  //Declared Vectors 
   std::vector<double> location = {42.651443, -73.749302};
-   std::vector<std::string> grocery = {"Hot Pepper Jam", "Dragon Fruit", "Brussel Sprouts"};
-  
-  std::cout << location.push_back(42.651443);
+  std::vector<double> delivery_order;
+  std::vector<std::string> grocery = {"Hot Pepper Jam", "Dragon Fruit", "Brussel Sprouts"};
+
+  //Vectors being manipulated
   location.push_back(42.651443);
   location.pop_back();
 
+  //Printing Vectors
   std::cout << "Size: " << location.size() << std::endl;
   std::cout << "First: " << location[0] << std::endl;
-  std::cout << "Second: " <<location[1] << std::endl;
-  std::cout << "Third: " <<location[2] << std::endl;
+  std::cout << "Second: " << location[1] << std::endl;
+  std::cout << "Third: " << location[2] << std::endl;
+
+  //Looping through grocery vector
+  for (int i = 0; i < grocery.size(); i++)
+  {
+    grocery[i] = "Hot Pepper Jam";
+    std::cout << grocery[i] << std::endl;
+  }
 
   return 0;
-
-
 }
