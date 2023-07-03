fbfd57ef7637f1aaef168e23718f2c402d64f9bb
carlos <115598773+maninthehood@users.noreply.github.com>
2023-07-03 03:27:17 +0000

    updated the file

--- app.cpp
+++ app.cpp
@@ -1,5 +1,21 @@
-#include <iostream>
+#include<iostream>
+#include<vector>
 
-int main() {
-  std::cout << "Hello World!\n";
+int main(){
+
+  std::vector<double> location = {42.651443, -73.749302};
+   std::vector<std::string> grocery = {"Hot Pepper Jam", "Dragon Fruit", "Brussel Sprouts"};
+  
+  std::cout << location.push_back(42.651443);
+  location.push_back(42.651443);
+  location.pop_back();
+
+  std::cout << "Size: " << location.size() << std::endl;
+  std::cout << "First: " << location[0] << std::endl;
+  std::cout << "Second: " <<location[1] << std::endl;
+  std::cout << "Third: " <<location[2] << std::endl;
+
+  return 0;
+
+
 }
